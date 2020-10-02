d9851ea2e99c97a15ddd1badec7db92b45984976
Wendy Grant <wendykgrant.tx@gmail.com>
2020-10-02 13:51:28 -0500

    Updates to pseudo

--- pseudo.js
+++ pseudo.js
@@ -1,16 +1,36 @@
-// Need : NYT API key
 
-//  moment for start/end year query
+// Links:
+    //  Bootstrap (Trisan)
+    //  Font awesome for icons (Myra)
 
 
-// querystring for "search term"
-
+// Sripts:
+    // <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    // <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    // <script>src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/locale/es-us.js"</script>
 
 // var:
-    // url -> https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=LYzWgxYCysNiAmcwGN5cXJnHCfBcsUYR
-    // queryString -> id="searchTerm"
-    // startYear - id= "startYear"
-    // endYear -> id="endYear"
-    // search -> id="searchBtn"
-    // clear -> id="clearBtn"
+    // Number of articles -> id = "searchNum"
+    // Search Term -> id="searchTerm"
+    // Start Year - id= "startYear"
+    // End Year -> id="endYear"
     
+// Events
+    // search -> id="searchBtn"
+    // drop-down -> id="numDropDown"
+    // clear -> id="clearBtn" 
+
+
+ 
+//The NY API request with the value of the search input.
+// Need : NYT API key
+// nytURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + articleRequest + '=election&api-key=LYzWgxYCysNiAmcwGN5cXJnHCfBcsUYR'
+
+//Sets the articles blank everytime a new search is started.
+
+
+
+//Appends the the Articles in a list.
+
+// If NY times request fails, throw an error message.
+
