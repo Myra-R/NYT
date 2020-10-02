82a3a58f2859210301c5948fc1fed9e0ccf79e26
Wendy Grant <wendykgrant.tx@gmail.com>
2020-10-02 13:30:38 -0500

    Added basic pseudo code

---
+++ pseudo.js
@@ -0,0 +1,16 @@
+// Need : NYT API key
+
+//  moment for start/end year query
+
+
+// querystring for "search term"
+
+
+// var:
+    // url -> https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=LYzWgxYCysNiAmcwGN5cXJnHCfBcsUYR
+    // queryString -> id="searchTerm"
+    // startYear - id= "startYear"
+    // endYear -> id="endYear"
+    // search -> id="searchBtn"
+    // clear -> id="clearBtn"
+    
